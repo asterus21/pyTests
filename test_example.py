2ae09ff256c1a00127ee1aa2c93e7de80fddf12c
Michael Paul <michaelpaulwem@gmail.com>
2026-05-21 08:09:30 +0300

    UI fixture added

--- test_example.py
+++ test_example.py
@@ -3,8 +3,8 @@ import pathlib
 import sys
 import pytest
 
-from polyanalyst6api import API
-from pa6webtest.locators.common import MAIN_MENU, MENU_LABEL, SCHEDULER_EVENTS_XPATH, SUBMENU_LIST
+from pa6webtest.locators.common import MAIN_MENU, MENU_LABEL, SCHEDULER_EVENTS_XPATH, SUBMENU_LIST, GRID_TABLE
+from pa6webtest.pages.pa6web import PA6Web
 
 
 REP_UUID = 'bc68a7bc-a5c4-4520-ae1e-8139d824bc45'
@@ -55,7 +55,8 @@ def test_add_task(open_scheduler, event_type):
     assert any(REP_UUID in str for str in content)
 
 
-@pytest.fixture(scope='module')
+# fixture to delete created tasks
+@pytest.fixture()
 def tasks_remove(open_scheduler):
     yield
     event_type = ['TimeEvent', 'SrvStartEvent']
@@ -67,6 +68,7 @@ def tasks_remove(open_scheduler):
         assert modal_window.is_visible()
         assert 'Are you sure you want to delete the selected item?' in modal_window.text_content()
         open_scheduler.confirm('Yes')
+    open_scheduler.waiting(3000)
 
 
 @pytest.mark.parametrize("task_name, order", [(task_name, 0), (task_name, 1)])
@@ -83,28 +85,28 @@ def test_publication_export(tasks_remove, open_scheduler, task_name, order):
     assert open_scheduler.page.locator('.spin').count() == 0
 
 
-@pytest.fixture(scope='module')
-def api():
-    with API(HOST, credentials['username'], credentials['password'], verify=False) as api:
-        yield api
-
-
-@pytest.fixture(scope='module')
-def files_remove(api):
-    files = []
-    yield files
-    for file in files:
-        api.request('/polyanalyst/api/v1.0/file/delete', method='post', json={"path": "@administrator@", "name": file})
-
-
-def test_publication_file_exported(api, files_remove):
-    home_folder = api.request('/polyanalyst/api/v1.0/folder/list', method='get',json=GET_FILES_JSON_DATA)
-    file_names = [item['name'] for item in home_folder[1]['items']]
+@pytest.mark.parametrize("menu, index", [
+    ("My applications", -3)
+])
+def test_files_exported(open_scheduler, menu, index, browser):
+    open_scheduler.main_menu_click(menu)    
+    submenu_items = open_scheduler.page.locator(MENU_LABELS_XPATH)
+    submenu_items.nth(index).click()
+    polyanalyst_drive = PA6Web((browser.pages[1]))
+    open_scheduler.page.close()
+    texts = polyanalyst_drive.page.locator(GRID_TABLE).last.inner_text()
+    items = texts.split('\n')
+    reports = [item for item in items if item.startswith(REPORT_NAME)]    
     etalon_path = (pathlib.Path(__file__).parents[1] / 'test_etalons' / 'Filter Upstream T49817.pdf')
-    etalon_name = etalon_path.name
-    for name in file_names:        
-        assert re.match(REPORT_REGEX, etalon_name)
-        files_remove.append(name)    
+    assert re.match(REPORT_REGEX, etalon_path.name)
+    for report in reports:
+        polyanalyst_drive.page.locator(f"text={report}").last.click(button='right')
+        polyanalyst_drive.page.locator(f"text='Delete...'").last.click()
+        modal_window = polyanalyst_drive.modal_window()
+        assert modal_window.is_visible()
+        assert f'Are you sure you want to delete {report}.pdf?' in modal_window.text_content()
+        polyanalyst_drive.confirm('Yes')        
+        polyanalyst_drive.waiting(1000)
 
 
 if __name__ == "__main__":
