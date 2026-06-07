7106ef512dbde6c50b953bb854cafe746d090411
Mike <70848378+asterus21@users.noreply.github.com>
2026-06-07 13:13:57 +0300

    Update test_example.py

--- test_example.py
+++ test_example.py
@@ -3,24 +3,25 @@ import pathlib
 import sys
 import pytest
 
-from pa6webtest.locators.common import MAIN_MENU, MENU_LABEL, SCHEDULER_EVENTS_XPATH, SUBMENU_LIST, GRID_TABLE
 from pa6webtest.pages.pa6web import PA6Web
+from pa6webtest.locators.common import text, MAIN_MENU, MENU_LABEL, SCHEDULER_EVENTS_XPATH, SUBMENU_LIST, GRID_TABLE
 
 
 REP_UUID = 'bc68a7bc-a5c4-4520-ae1e-8139d824bc45'
 PRJ_UUID = 'c6c298c2-419a-480e-b815-5511847125d4'
 REPORT_NAME = 'Filter Upstream T49817'
 REPORT_REGEX = fr'{REPORT_NAME}.*\.pdf'
-GET_FILES_JSON_DATA = {"path": "@administrator@", "mask": "|pdf"}
-HOST = 'https://localhost:5043'
 
 MENU_LABELS_XPATH = MAIN_MENU + SUBMENU_LIST + MENU_LABEL
 
-open_report_actions = ('Add task', 'Export publication to file', 'Root', REPORT_NAME, 'Open')
 add_task_actions = ('Test (immediate execution)', )
 export_formats = ('PNG', 'JPG', 'PDF', 'PPTX', 'ZIP')
 task_name = f"Export '{REPORT_NAME}' ({REP_UUID}) report publication to file"
-credentials = {'username': 'administrator', 'password': ''}
+
+
+# Scheduler method
+def get_scheduler_events(open_scheduler):
+    return open_scheduler.locator(SCHEDULER_EVENTS_XPATH)
 
 
 @pytest.fixture(scope='module')
@@ -29,74 +30,47 @@ def publication_create(open_report, browser):
     browser.pages[-1]
 
 
-@pytest.mark.parametrize("menu, sub_menu_index, name", [("Add event", 0, "TimeEvent")])
-def test_add_event(publication_create, open_scheduler, menu, sub_menu_index, name):
-    open_scheduler.main_menu_click(menu)
-    submenu_items = open_scheduler.page.locator(MENU_LABELS_XPATH)
-    submenu_items.nth(sub_menu_index).click()
-    open_scheduler.waiting(3000)
-    events = open_scheduler.page.locator(SCHEDULER_EVENTS_XPATH).all_inner_texts()
-    		    assert name in events[0].split('\n')
-    @pytest.mark.parametrize("event_type", [('TimeEvent')])
+def test_add_event(publication_create, open_scheduler):
+    open_scheduler.main_menu_click('Add event > Add event to execute at specified time')
+    events = get_scheduler_events(open_scheduler).inner_text()
+    assert "TimeEvent" in events
 
 
-def test_add_task(open_scheduler, event_type):
-        open_scheduler.page.locator(f"text={event_type}").first.click()
-    open_scheduler.page.locator(f"text={event_type}").first.click(button='right')
-    for action in open_report_actions[:-1]:
-        open_scheduler.page.locator(f"text={action}").last.click()
-    open_scheduler.page.locator(f"text={open_report_actions[-1]}").last.click()
-    open_scheduler.page.locator(f"text={event_type}").first.click()
-    open_scheduler.waiting(3000)
-    content = open_scheduler.page.locator(SCHEDULER_EVENTS_XPATH).all_inner_texts()
-    assert any(REP_UUID in str for str in content)
+def test_add_task(open_scheduler):
+    all_events = get_scheduler_events(open_scheduler)
+    time_event_locator = all_events.locator(text('TimeEvent'))
+    time_event_locator.first.context_menu_click('Add task > Export publication to file')
+    modal_window = open_scheduler.modal_window()
+    modal_window.locator(text('Root')).last.click()
+    modal_window.locator(text(REPORT_NAME)).last.click()
+    modal_window.button('Open').click()    
+    time_event_locator.first.click()
+    content = all_events.inner_text()
+    assert REP_UUID in content
 
 
 # fixture to delete created tasks
 @pytest.fixture()
 def tasks_remove(open_scheduler):
     yield
-    event_type = ['TimeEvent']
-    for event in event_type:
-        open_scheduler.page.locator(f"text={event}").first.click()
-                open_scheduler.page.locator(f"text={event}").first.click(button='right')
-        open_scheduler.page.locator("text='Delete'").last.click()
-        modal_window = open_scheduler.modal_window()
-        assert modal_window.is_visible()
-        assert 'Are you sure you want to delete the selected item?' in modal_window.text_content()
-        open_scheduler.confirm('Yes')
-    open_scheduler.waiting(3000)
-
-
-@pytest.mark.parametrize("task_name, order", [(task_name, 0)])
-def test_publication_export(tasks_remove, open_scheduler, task_name, order):
-    	    open_scheduler.page.locator(f"text={task_name}").nth(order).click()
-    open_scheduler.page.locator('//*[@class="group"]').click()
-    open_scheduler.page.locator(f"text={export_formats[2]}").first.click()
-    open_scheduler.page.locator(f"text={task_name}").nth(order).click(button='right')
-    open_scheduler.page.locator(f"text={add_task_actions[0]}").first.click()
-    open_scheduler.waiting(3000)
-    amount_of_arrow_icons = open_scheduler.page.locator('.spin').count()
-    for i in range(amount_of_arrow_icons):
-        open_scheduler.page.locator('.spin').nth(i).wait_for(state='detached', timeout=0)
-    assert open_scheduler.page.locator('.spin').count() == 0
+    open_scheduler.locator(text('TimeEvent')).context_menu_click('Delete')
+    modal_window = open_scheduler.modal_window()
+    assert modal_window.is_visible()
+    assert 'Are you sure you want to delete the selected item?' in modal_window.text_content()
+    open_scheduler.confirm('Yes')
+    open_scheduler.waiting(1000)
 
 
 # fixture to delete exported files
 @pytest.fixture()
 def files_remove(open_scheduler, browser):
     yield
-    # open_scheduler.button('Main menu').click()
-    open_scheduler.main_menu_click('My applications')
-    submenu_items = open_scheduler.page.locator(MENU_LABELS_XPATH)    
-    submenu_items.nth(-3).click()
+    open_scheduler.main_menu_click('My applications > PolyAnalyst Drive')
     polyanalyst_drive = PA6Web((browser.pages[1]))
-    texts = polyanalyst_drive.page.locator(GRID_TABLE).last.inner_text()
-    items = texts.split('\n')
-    reports = [item for item in items if item.startswith(REPORT_NAME)]
+    files = polyanalyst_drive.locator(GRID_TABLE).last.inner_text().split('\n')
+    reports = [file for file in files if file.startswith(REPORT_NAME)]
     for report in reports:
-        polyanalyst_drive.page.locator(f"text={report}").last.click(button='right')
-        polyanalyst_drive.page.locator(f"text='Delete...'").last.click()
+        polyanalyst_drive.locator(text(report)).last.context_menu_click('Delete...')
         modal_window = polyanalyst_drive.modal_window()
         assert modal_window.is_visible()
         assert f'Are you sure you want to delete {report}.pdf?' in modal_window.text_content()
@@ -105,7 +79,14 @@ def files_remove(open_scheduler, browser):
     polyanalyst_drive.page.close()
 
 
-def test_files_exported(files_remove):    
+def test_publication_export(open_scheduler, tasks_remove, files_remove):
+    task_name = f"Export '{REPORT_NAME}' ({REP_UUID}) report publication to file"
+    open_scheduler.locator(text(task_name)).first.click()
+    open_scheduler.locator(text('PNG')).first.click()
+    open_scheduler.locator(text('PDF')).first.click()
+    open_scheduler.locator(text(task_name)).first.context_menu_click('Test (immediate execution)')    
+    open_scheduler.locator('.spin').object.wait_for(state='detached', timeout=0)
+    assert open_scheduler.locator('.spin').is_hidden()
     etalon_path = (pathlib.Path(__file__).parents[1] / 'test_etalons' / 'Filter Upstream T49817.pdf')
     assert re.match(REPORT_REGEX, etalon_path.name)
 
